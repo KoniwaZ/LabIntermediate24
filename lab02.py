4811aef092c0dab25bbde54bc4c79afb71413aa6
student <student@students-iMac.local>
2025-01-23 14:01:13 +0700

    push lab02 to repo

--- lab02.py
+++ lab02.py
@@ -1,5 +1,6 @@
-word = "Prasmul".lower()
+word = "pmbs".lower()
 guess = ["_"] * len(word)
+guessed = []
 chance = 6
 
 for char in guess:
@@ -10,15 +11,22 @@ while chance > 0:
     print("Chance left:", chance)
     guessing = input("Type one character: ").lower()
     
+    if guessing in guessed:
+        print("You already guessed this character. Try another one.")
+        continue
+    else:
+        guessed.append(guessing)
+    
     if guessing in word:
         for i in range(len(word)):
             if word[i] == guessing:  
                 guess[i] = guessing
                 print("You right! Continue guessing")
         
-        for char in guess :
+        for char in guess:
             print(char, end=" ")
         print()
+
         chance -= 1
         
         if "_" not in guess:
@@ -27,6 +35,5 @@ while chance > 0:
     else:
         print("Wrong. Guess another character.")
         chance -= 1
-
 if chance == 0:
-    print("You lose. The word is :", word)
+    print("You lose, out of chance. The word is :", word)
