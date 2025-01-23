24167fea6895cc9c393e7069b74e480fa2c10955
student <student@students-iMac.local>
2025-01-23 14:44:06 +0700

    Push Hangman2 to Repo

--- lab02.py
+++ lab02.py
@@ -1,39 +1,43 @@
-word = "pmbs".lower()
+word = "qwerty".lower()
 guess = ["_"] * len(word)
 guessed = []
 chance = 6
 
-for char in guess:
-    print(char, end=" ")
-print()
 
 while chance > 0:
+    for char in guess:
+        print(char, end=" ")
+    print()
     print("Chance left:", chance)
     guessing = input("Type one character: ").lower()
-    
-    if guessing in guessed:
-        print("You already guessed this character. Try another one.")
-        continue
-    else:
-        guessed.append(guessing)
-    
-    if guessing in word:
-        for i in range(len(word)):
-            if word[i] == guessing:  
-                guess[i] = guessing
-                print("You right! Continue guessing")
+    alpha = guessing.isalpha()
+    if alpha == True :
+        if guessing in guessed:
+            print("You already guessed this character. Try another one.")
+            continue
+        else:
+            guessed.append(guessing)
         
-        for char in guess:
-            print(char, end=" ")
-        print()
+        if guessing in word:
+            for i in range(len(word)):
+                if word[i] == guessing:  
+                    guess[i] = guessing
+                    print("You right! Continue guessing")
 
-        chance -= 1
-        
-        if "_" not in guess:
-            print("Completed! The word is :", word)
-            break
-    else:
-        print("Wrong. Guess another character.")
-        chance -= 1
-if chance == 0:
+            chance -= 1
+            
+            if "_" not in guess:
+                print("Completed! The word is :", word)
+                break
+        else:
+            chance -= 1
+            if chance > 0:
+                print("Wrong. Guess another character.")
+            else:
+                print("You lose, out of chance. The word is :", word)
+                break
+    else :
+        print("Invalid input. Try again.")
+              
+if chance <= 0:
     print("You lose, out of chance. The word is :", word)
